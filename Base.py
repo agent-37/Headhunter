0f178f797e0fac6c6530f47a284af8b7bcc16622
maxim dementyev <maxim.dementev.04@gmail.com>
2023-04-21 21:05:40 +0300

    рефакторинг кода, создана функция add_new_profile(стоит сделать проверку)

--- Base.py
+++ Base.py
@@ -1,19 +1,11 @@
 import Profile
-import Specialization
 
 
 class Base:
     # это база профилей(анкет), добавленных на сайт
     data_base = []
+    # это видимая(отфильтрованная) база профилей(анкет), добавленных на сайт и
     visible_data_base = []
-    def add_new_profile(self, name: str, placer: str, spec: list[Specialization], salary: int, place_work: list, \
-                        stars=None) -> bool:
-        new_profile = Profile(name, placer, spec, salary, place_work, stars)
-        data_base.append(new_profile)
-
-
-
-
-
-
-
+    def add_new_profile(self, new_profile: Profile):
+        # !!здесь стоит сделать проверку на то, что профиль хороший
+        self.data_base.append(new_profile)
