856e5be95d9dbd8f7b6e50d7c83afc1be6dd8045
witcharm <yuliya.nikulina.25.04@mail.ru>
2023-04-24 22:09:15 +0300

    функция добавления специализации

---
+++ List_of_specialization.py
@@ -0,0 +1,12 @@
+import Specialization
+
+class List_of_specialization:
+    specializations = []
+
+    def add_specialization(self, new_spec: Specialization):
+        # проверка на то, есть ли уже эта специализация в листе
+        # !!здесь стоит сделать проверку на то, что специализыция введена корректно (под вопросом)
+        if new_spec not in self.specializations:
+            self.specializations.append(new_spec)
+        # !!иначе ошибка (потом сделать или не сделать)
+
