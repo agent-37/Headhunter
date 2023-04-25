54e46ef917a5174857c42d979faeda3526e92adb
Maxim Dementyev <maxim.dementev.04@gmail.com>
2023-04-25 17:28:00 +0300

    переопределил метод ==, так как in не работает

--- specialization.py
+++ specialization.py
@@ -4,6 +4,14 @@ class Specialization:
     # Класс профессии. Состоит из списка для сужения специализации
     def __init__(self, profession: list[str]):
         self.profession = profession
+    def __eq__(self, other):
+        # переопределил метод ==, так как in не работает
+        if len(self.profession) != len(other.profession):
+            return False
+        for i in range(len(self.profession)):
+            if self.profession[i] != other.profession[i]:
+                return False
+        return True
 
     def is_nested(self, new_profession) -> bool:
         # Функция проверят вложена ли new_profession в данную profession
