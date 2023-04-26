430e076f1a33db3806c3ab809a6f42e4f21f7290
Maxim Dementyev <maxim.dementev.04@gmail.com>
2023-04-26 12:38:37 +0300

    добавил комментарий

--- base.py
+++ base.py
@@ -8,3 +8,6 @@ class Base:
     def add_new_profile(self, new_profile: Profile):
         # !!здесь стоит сделать проверку на то, что профиль хороший
         self.data_base.append(new_profile)
+
+    def check_correct_specialization_in_profile(self):
+        # проверка наличия введённой специализации в базе данных (того, что такая вообще существует)
