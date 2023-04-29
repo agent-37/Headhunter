9f8ca8e99af71f07924c6cea2a91912db38fb968
Maxim Dementyev <maxim.dementev.04@gmail.com>
2023-04-29 17:24:20 +0300

    разбили функцию read_profile_from_file на несколько меньших

--- base.py
+++ base.py
@@ -7,10 +7,13 @@ class Base:
     # это база профилей(анкет), добавленных на сайт
     data_base = []
 
-    def add_new_profile(self, new_profile: Profile):
-        # !!здесь стоит сделать проверку на то, что профиль хороший
-        self.data_base.append(new_profile)
-        # заглушка !!!!!!!!
+    def add_new_profile(self, new_profile: Profile, list_of_spec: List_of_specialization):
+        # добавление нового профиля, если он пройдёт проверку
+        if new_profile.check_all_spec_correct(list_of_spec):
+            self.data_base.append(new_profile)
+        else:
+            with open('exeptions.txt', 'a') as exeption_file:
+                exeption_file.write('Найден человек: ' + new_profile.name + 'с неопознанной специализацией' + '\n')
 
     def read_profile_from_file(self, list_of_spec: List_of_specialization):
         f_prof = open('input_file_of_profiles.txt')
@@ -40,17 +43,7 @@ class Base:
                 stars.append(f_prof.readline())
             new_prof = Profile(name, placer, spec, salary, place_work, name_education, level_education, unique_skills,
                                stars)
-            if len(new_prof.spec) == 0:
-                continue
-            flag = 0
-            for spec in new_prof.spec:
-                if spec not in list_of_spec.specializations:
-                    flag = 1
-                    break
-
-            if flag == 0:
-                self.data_base.append(new_prof)
+            self.add_new_profile(new_prof, list_of_spec)
         f_prof.close()
-
 
 
