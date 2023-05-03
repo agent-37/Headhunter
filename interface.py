6ef3e65909f908c3ea39e10f2099adabb16cd1b6
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2023-05-03 20:09:42 +0300

    Разделил подгрузку файлов от конструктора( хочется уметь создавать пустой интерфейс)

--- interface.py
+++ interface.py
@@ -9,9 +9,11 @@ class Interface:
     def __init__(self):
         # Конструктор для класса. Нам потребуется база данных, список всех профессий и фильтр
         self.all_spec = List_of_specialization()
-        self.all_spec.read_from_file()
         self.data_base = Base()
-        # ?? Какие параметры нужны для чтения из файла. (Например вроде нужен list_of_spec, так как  надо
-        # почему-то проверять, что анкета хорошая или плохая)
-        # self.data_base.read_from_file()
         self.filter = Filter()
+
+    def upload_base_from_file(self):
+        self.data_base.read_profile_from_file(self.all_spec)
+
+    def upload_spec_from_file(self):
+        self.all_spec.read_from_file()
