7922393a7df3ee33e62c86d589163e82d08671ef
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2023-05-07 22:42:23 +0300

    Сделал Функцию чтения анкеты из консоли(Сделана без хорошей тестировки)

--- profile.py
+++ profile.py
@@ -3,6 +3,28 @@ from specialization import Specialization
 from list_of_specialization import List_of_specialization
 
 
+def is_float(element: any) -> bool:
+    # Проверка, того что элемент это число с плавающей запятой
+    if element is None:
+        return False
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+
+def is_int(element: any) -> bool:
+    # Проверка, того что элемент это целое число
+    if element is None:
+        return False
+    try:
+        int(element)
+        return True
+    except ValueError:
+        return False
+
+
 class Profile:
     # Это класс в котором будет храниться информация о профиле вакансии
     # или человека ищущего работу
@@ -70,3 +92,69 @@ class Profile:
                 return False
 
         return True
+
+    def read_from_console(self, list_of_spec: List_of_specialization):
+        self.name = input('Ведите ФИО или название вашей Организации\n')
+        placer = input('Ведите "соискатель" или "организация"\n')
+        while placer != 'соискатель' and placer != 'организация':
+            placer = input('Ведите "соискатель" или "организация"\n')
+            print(placer != 'соискатель', placer != 'организация')
+        if placer == "организация":
+            self.placer = 1
+        else:
+            self.placer = 0
+
+        buff = input('Ведите количество специализаций\n')
+        while not is_int(buff):
+            buff = input('Количество специализаций должно быть целым\n')
+        buff = int(buff)
+        if self.placer == 1:
+            for i in range(buff):
+                spec_name = list(input('Введите название специализации через запятую(", ")\n').split(', '))
+                min_exp = input('Ведите минимальный стаж работы(если эта информация не важна, то введите 0)\n')
+                while not is_float(min_exp):
+                    min_exp = input('Минимальный стаж работы - это вещественное число\n')
+                min_exp = float(min_exp)
+                max_exp = input('Ведите максимальный стаж работы(если эта информация не важна, то введите 100)\n')
+                while not is_float(max_exp) or float(max_exp) < min_exp:
+                    max_exp = input('Максимальный стаж работы - это вещественное число не меньшее минимального стажа\n')
+                max_exp = float(max_exp)
+                new_spec = Specialization(spec_name, min_exp, max_exp)
+                while new_spec not in list_of_spec.specializations:
+                    print('Специализации нет в базе данных, попробуйте ввести ее по другому')
+                    new_spec.profession = list(
+                        input('Введите название специализации через запятую(", ")\n').split(', '))
+        else:
+            for i in range(buff):
+                spec_name = list(input('Введите название специализации через запятую(", ")\n').split(', '))
+                min_exp = input('Ведите стаж работы\n')
+                while not is_float(min_exp):
+                    min_exp = input('Стаж работы - это вещественное число\n')
+                min_exp = float(min_exp)
+                new_spec = Specialization(spec_name, min_exp, min_exp)
+                while new_spec not in list_of_spec.specializations:
+                    print('Специализации нет в базе данных, попробуйте ввести ее по другому')
+                    new_spec.profession = list(
+                        input('Введите название специализации через запятую (", ")\n').split(', '))
+        self.salary = input('Ведите зарплату\n')
+        while not is_float(buff):
+            self.salary = input('Зарплата должна быть вещественной\n')
+        self.salary = int(self.salary)
+        self.place_work = list(input('Введите через запятую места работы\n').split(', '))
+        if self.placer == 0:
+            self.name_education = input('Введите ваше образование\n')
+        # !! Здесь нужно будет выводить степени образования чтобы пользователь сам их определил
+        self.level_education = input('Ведите степень образования\n')
+        while not is_int(self.level_education):
+            self.level_education = input('Степень образования быть целой\n')
+        self.level_education = int(self.level_education)
+        self.unique_skills = list(input('Введите через запятую уникальные умения или возможности\n' +
+                                        'Например: Водительские права категории\n' +
+                                        'Если нет таковых оставьте это поле пустым').split(', '))
+
+
+l = List_of_specialization()
+l.read_from_file()
+proba  = Profile('123', '312',[],123,[],'123',123,[],123)
+proba.read_from_console(l)
+
